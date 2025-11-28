7f78664dbe067eab020dab24bf34aac880f716b3
Jiayin He <90810668+7niyaij@users.noreply.github.com>
2025-11-28 12:14:41 -0700

    Improve the speaker diarlization code for executing multiple files at the same time

--- src/speaker_diarlization.py
+++ src/speaker_diarlization.py
@@ -4,11 +4,11 @@ from pyannote.audio.pipelines.utils.hook import ProgressHook
 import pandas as pd
 from pydub import AudioSegment
 from pathlib import Path
-import os
+import os, glob
 import argparse
 
 
-def pyannote_speaker_diarlization(audio_file, output_file):
+def pyannote_speaker_diarlization(audio_file, output_path):
     # Community-1 open-source speaker diarization pipeline
     with open("hf_token.txt", "r") as f:
         HF_TOKEN = f.read().strip()
@@ -30,6 +30,7 @@ def pyannote_speaker_diarlization(audio_file, output_file):
         segments.append({
             "start_time": round(turn.start, 3),
             "end_time": round(turn.end, 3),
+            "duration": round(turn.end - turn.start, 3),
             "speaker": speaker
         })   
 
@@ -39,6 +40,7 @@ def pyannote_speaker_diarlization(audio_file, output_file):
     for segment in segments[1:]:
         if segment["speaker"] == current["speaker"]:
             current["end_time"] = segment["end_time"]
+            current["duration"] += segment["duration"]
         else:
             merged_segments.append(current)
             current = segment
@@ -46,24 +48,41 @@ def pyannote_speaker_diarlization(audio_file, output_file):
 
     # save the merged segments to csv file
     df = pd.DataFrame(merged_segments)
-    df.to_csv(output_file, index=False)
+    df.to_csv(os.path.join(output_path, "speaker_diarlization_results.csv"), index=False)
 
-def audio_segment(audio_file, transcript_path):
-    transcript = pd.read_csv(transcript_path, index_col=False)
-    for index, row in transcript.iterrows():
-        start = float(row['start_time']) * 1000
-        stop = float(row['end_time']) * 1000
+
+def audio_segment(audio_file, output_path):
+    audio_file = Path(audio_file)
+    output_path = Path(output_path)
+    csv_path = output_path / "speaker_diarlization_results.csv"
+    speaker_diarlization_results_df = pd.read_csv(csv_path)
+    audio = AudioSegment.from_wav(str(audio_file))
+    
+    for index, row in speaker_diarlization_results_df.iterrows():
+        start_time = float(row['start_time']) * 1000
+        end_time = float(row['end_time']) * 1000
         # print(str(start)+"-"+str(stop))
-        new_audio_file = audio_file.split('_')[0] + '_' + str(index + 1).zfill(3) + '.wav'
-        audio = AudioSegment.from_wav(f"./{audio_file}")
-        audio_segment = audio[start:stop]
-        destination = Path(f"./{audio_file.split('_')[0]}_segments")
-        Path.mkdir(destination, parents=True, exist_ok=True)
-        audio_segment.export(os.path.join(destination,new_audio_file), format="wav")
+
+        # create output name
+        new_name = f"{audio_file.stem}_{str(index + 1).zfill(3)}.wav"
+
+        # cut segement
+        audio_segment = audio[start_time:end_time]
+
+        # Save segment directly into output_path
+        segment_path = output_path / new_name
+        audio_segment.export(segment_path, format="wav")
+        print(f"Saved: {segment_path}")
 
 def main(input_dir, output_dir):
-    pyannote_speaker_diarlization(input_dir, output_dir)
-    audio_segment(input_dir, output_dir)
+    # for file in glob.glob(os.path.join(input_dir, '*.wav')):
+    for file in [os.path.join(input_dir, "PAR1 W5.wav")]:
+        file_basename = os.path.basename(file).split('.')[0]
+        file_output_dir = os.path.join(output_dir, file_basename)
+        Path(file_output_dir).mkdir(parents=True, exist_ok=True)
+
+        # pyannote_speaker_diarlization(file, file_output_dir)
+        audio_segment(file, file_output_dir)
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
@@ -76,5 +95,17 @@ if __name__ == '__main__':
     output_dir = args.output
     is_recursive = args.recursive
 
-    input_dir = "PAR1_Week3.wav"
-    output_dir = "PAR1_Week3_diarization.csv"
+    input_dir = "D:\\Study\\eMPowerProject\\check_in_recordings_wav_cleaned\\denoised_and_normalized"
+    output_dir = "D:\\Study\\eMPowerProject\\results"
+    is_recursive = True
+
+    if is_recursive:
+        for subject in ["PAR1"]:
+        # for subject in os.listdir(input_dir):
+            subject_input_dir = os.path.join(input_dir, subject)
+
+            if os.path.isdir(subject_input_dir):
+                subject_outpu_dir = os.path.join(output_dir, subject)
+                main(subject_input_dir, subject_outpu_dir)
+    else:
+        main(input_dir, output_dir)
