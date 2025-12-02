5effc4398e5b12536982e77b67014ec1109e439d
Jiayin He <jiayin7@ualberta.ca>
2025-12-02 14:27:24 -0700

    Fix hf token error

--- src/speaker_diarlization.py
+++ src/speaker_diarlization.py
@@ -18,7 +18,7 @@ def pyannote_speaker_diarlization(audio_file, output_path):
 
     pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization-community-1",
-        token=HF_TOKEN)
+        use_auth_token=HF_TOKEN)
 
     # send pipeline to GPU (when available)
     # pipeline.to(torch.device("cuda"))
